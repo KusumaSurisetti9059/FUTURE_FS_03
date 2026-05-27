8ffb441de194bebff6b69e1a5b5812cd382219eb
KusumaSurisetti <n230133@rguktn.ac.in>
2026-05-27 17:09:12 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function showProducts(){
-            let products=document.getElementById("more_products");
-            if(products.style.display==="grid"){
-                products.style.display="none";
-            }
-            else{
-                products.style.display="grid";
-            }
-        }
